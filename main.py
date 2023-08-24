bc6bff53c7796bb682595a21abea85a5eb23e887
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2023-08-24 23:28:58 +0300

    input and output

---
+++ main.py
@@ -0,0 +1,11 @@
+# Lesson topic: data input-output
+print('Hello, World!')
+
+
+print('What is you name?')
+name = input()
+print('Hi,', name)
+
+
+another_name = input('Input you name')
+print('Hello,', another_name)
