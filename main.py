f2f21a6a025ce2d3eb96a915cab43aea6fb150d5
Alex Selyutin <65799598+JuniorQAengineer@users.noreply.github.com>
2023-09-02 14:30:53 +0300

    Nested and cascading conditions. Solution examples

--- main.py
+++ main.py
@@ -378,3 +378,46 @@ if (column_out <= column_in + 1 and column_out >= column_in - 1) and (line_out <
     print('YES')
 else:
     print('NO')
+
+# Даны три целых числа. Определите, сколько среди них совпадающих. Программа должна вывести одно из чисел: 3 (если все совпадают), 2 (если два совпадает) или 0 (если все числа различны).
+# 1 способ. Использование вложенного условного оператора.
+a, b, c = int(input()), int(input()), int(input())
+
+if a == b:
+    if b == c:
+        print(3)
+    else:
+        print(2)
+else:
+    if a == c:
+        print(2)
+    else:
+        if b == c:
+            print(2)
+        else:
+            print(0)
+
+# 2 способ. Использование каскадного условного оператора.
+a, b, c = int(input()), int(input()), int(input())
+
+if a == b == c:
+    print(3)
+elif a == b != c:
+    print(2)
+elif a != b == c:
+    print(2)
+elif a == c != b:
+    print(2)
+else:
+    print(0)
+
+# 3 способ. Использование каскадного условного оператора и логического оператора or.
+a, b, c = int(input()), int(input()), int(input())
+
+if a == b == c:
+    print(3)
+elif a == b != c or a != b == c or a == c != b:
+    print(2)
+else:
+    print(0)
+
